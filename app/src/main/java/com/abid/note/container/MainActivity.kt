b8616e739055ce565c915fa17047a1db7b2a125d
Abid Hasan <abid@field.buzz>
2019-06-09 15:16:51 +0600

    Added notebook section
    - added fragments for notes, notebooks, noteDetails
    - added adapter for notebook section
    #TODO-fix 
    - fix add notbook button size

--- app/src/main/java/com/abid/note/container/MainActivity.kt
+++ app/src/main/java/com/abid/note/container/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.abid.note
+package com.abid.note.container
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.abid.note.R
 
 class MainActivity : AppCompatActivity() {
 
