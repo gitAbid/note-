afa594e663acb1c5c66d857f200455cd5a09c375
Abid Hasan <abid@field.buzz>
2019-06-09 15:16:51 +0600

    Added notebook section
    - added fragments for notes, notebooks, noteDetails
    - added adapter for notebook section
    TODO-FIX 
    - fix add notebook button size

---
+++ app/src/main/java/com/abid/note/ui/notes/adapter/NotebookAdapter.kt
@@ -0,0 +1,74 @@
+package com.abid.note.ui.notes.adapter
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.abid.note.R
+import com.abid.note.ui.model.NotebookModel
+import kotlinx.android.synthetic.main.item_add_notebook.view.*
+import kotlinx.android.synthetic.main.item_notebook.view.*
+
+/**
+ * Created by Abid Hasan on 6/9/19.
+ * Copyright (c) 2019. All rights reserved.
+ * @author Abid Hasan
+ *
+ */
+
+class NotebookAdapter(var items: List<NotebookModel>) : RecyclerView.Adapter<NotebookAdapter.ViewHolder>() {
+
+    private val FIRST_INDEX: Int = 0
+    lateinit var context: Context
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onBindViewHolder(viewholder: ViewHolder, position: Int) {
+
+        if (position == FIRST_INDEX) {
+            viewholder.addNotebook?.setOnClickListener {
+                Toast.makeText(context, "Add Item Clicked", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            viewholder.notebookTitle?.text = items?.get(position)?.title
+            viewholder.notbookItem?.setOnClickListener {
+                Toast.makeText(context, "Notebook Item Clicked", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return position
+    }
+    override fun onCreateViewHolder(viewholder: ViewGroup, position: Int): ViewHolder {
+        context = viewholder.context
+        if (position == FIRST_INDEX) {
+            return ViewHolder(
+                LayoutInflater.from(viewholder.context).inflate(
+                    R.layout.item_add_notebook,
+                    viewholder,
+                    false
+                )
+            )
+        } else {
+            return ViewHolder(
+                LayoutInflater.from(viewholder.context).inflate(
+                    R.layout.item_notebook,
+                    viewholder,
+                    false
+                )
+            )
+        }
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val addNotebook = view.ivAddNotebook
+        val notbookItem = view.llNotebookItem
+        val notebookTitle = view.tvNotebookTitle
+    }
+}
+
---
+++ app/src/main/java/com/abid/note/ui/notes/fragments/NotesFragment.kt
@@ -0,0 +1,40 @@
+package com.abid.note.ui.notes.fragments
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.abid.note.R
+import com.abid.note.ui.model.NotebookModel
+import com.abid.note.ui.notes.adapter.NotebookAdapter
+import kotlinx.android.synthetic.main.fragment_notes.*
+
+class NotesFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_notes, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        notebookRecyclerView.apply {
+            layoutManager = LinearLayoutManager(activity, LinearLayoutManager.HORIZONTAL, false)
+            setHasFixedSize(true)
+            adapter = NotebookAdapter(
+                listOf(
+                    NotebookModel("Abid", 1, listOf(1, 2)),
+                    NotebookModel("Abid", 1, listOf(1, 2)),
+                    NotebookModel("Abid", 1, listOf(1, 2)),
+                    NotebookModel("Abid", 1, listOf(1, 2))
+                )
+            )
+
+        }
+    }
+
+}
